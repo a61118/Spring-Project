db31956ff6acb2a9d43a4153f1e338d708d98abb
Filipe Pereira <1999filipepereira@gmail.com>
2022-06-20 19:38:35 +0100

    minor changes

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -1,6 +1,7 @@
 package com.example.demo.student;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
     private final StudentService studentService;
 
+    @Autowired
     public StudentController(StudentService studentService){
         this.studentService = studentService;
     }
--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -2,7 +2,9 @@ package com.example.demo.student;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import org.springframework.stereotype.Service;
 
+@Service
 public class StudentService {
     
     public List<Student> getStudents(){
